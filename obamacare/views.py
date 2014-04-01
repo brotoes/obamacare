4a2706a7b44a73fc30140ecdaaf2d61a3cc7735b
brotoes <btoews@ualberta.ca>
2014-03-31 18:54:40 -0600

    first diagnosis returned for reports

--- obamacare/views.py
+++ obamacare/views.py
@@ -782,6 +782,7 @@ def report(request):
                     i[0].address,
                     i[0].phone,
                     i[1].test_date,
+                    i[1].diagnosis,
                     ))
     def append_(item):
         data.append(item)
@@ -805,6 +806,7 @@ def report(request):
                  'Address',
                  'Phone',
                  'Test Date',
+                 'Diagnosis',
                  ),
        data=data,
        filter=diag_filter,
